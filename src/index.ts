036df761b725026471f49f7487efa813f22e06db
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-10-15 16:36:59 +0300

    [typescript] Initialized.

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+/*
+ * Author: Danil Andreev | danssg08@gmail.com | https://github.com/DanilAndreev
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Danil Andreev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import * as TelegramBot from "node-telegram-bot-api";
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+if (!token)
+    throw new Error(`FatalError: you must specify token to run this app! "token" = "${token}".`);
+
+const Bot = new TelegramBot(token, {polling: true});
+
+Bot.onText(/\/echo (.+)/, (message, match) => {
+    const fromId = message.from.id;
+    let response = match[1];
+    Bot.sendMessage(fromId, response).then();
+});
